8386b23eb4f0417c724e1ec4f8e9feedc86f05dc
michelediss <mic.paolino@gmail.com>
2025-03-21 22:20:17 +0100

    test commit

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ read commit_message
 
 # 3. Committa e pusha tutto il contenuto dell'app tranne la cartella dist sul ramo main
 git add .
-git reset dist  # Rimuove dist dall'indice, se già aggiunto
+git reset dist  # Rimuove dist dall'indice, in modo che non venga incluso nel commit per main
 git commit -m "$commit_message"
 git push origin main
 
 # 4. Committa e pusha la cartella dist sul ramo gh-pages usando subtree
-git subtree push --prefix dist origin gh-pages -m "$commit_message"
+git subtree push --prefix dist origin gh-pages
